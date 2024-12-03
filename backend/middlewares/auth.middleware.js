e58db58aa06316efaf716ecafc0a41f090d11a74
Aviraj0714 <131906596+Aviraj0714@users.noreply.github.com>
2024-12-04 02:00:06 +0530

    captain autharization

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,25 +1,59 @@
-const userModel = require("../models/user.model");
+module.exports.authUser = async (req, res, next) => {
+    let token = req.cookies.token;
 
-const bcrypt=require('bcrypt');
-const jwt = require('jsonwebtoken');
-
-
-module.exports.authUser=async(req,res,next)=>{
-    const token=req.cookies.token||req.headers.authorization.split('')[1];
-    if(!token){
-        return res.status(401).json({message:"Unauthorized"}) ;
+    if (!token && req.headers.authorization) {
+        // Extract token from the Bearer header
+        const authHeader = req.headers.authorization;
+        if (authHeader.startsWith("Bearer ")) {
+            token = authHeader.split(' ')[1];
+        }
     }
-const isBlacklisted=await userModel.findOne({token:token});
-if(isBlacklisted){
-    return res.status(401).json({message:"Unauthorized"}) 
-}
 
-    try{
-        const decoded=jwt.verify(token,process.env.JWT_SECRET);
-        const user=await userModel.findById(decoded._id);
-        req.user=user;
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const isBlacklisted = await blackListTokenModel.findOne({ token });
+    if (isBlacklisted) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await userModel.findById(decoded._id);
+        req.user = user;
         return next();
-    }catch (err){
-        return res.status(401).json({message:"unautharized"}) ;
+    } catch (err) {
+        return res.status(401).json({ message: "Unauthorized" });
     }
-}
+};
+
+module.exports.authCaptain = async (req, res, next) => {
+    let token = req.cookies.token;
+
+    if (!token && req.headers.authorization) {
+        // Extract token from the Bearer header
+        const authHeader = req.headers.authorization;
+        if (authHeader.startsWith("Bearer ")) {
+            token = authHeader.split(' ')[1];
+        }
+    }
+
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const isBlacklisted = await blackListTokenModel.findOne({ token });
+    if (isBlacklisted) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const captain = await captainModel.findById(decoded._id);
+        req.captain = captain;
+        return next();
+    } catch (err) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+};
