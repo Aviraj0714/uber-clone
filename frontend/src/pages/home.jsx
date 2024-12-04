061e0d5a5ffc43017c687fb38482a8ffd26f3e14
Aviraj0714 <131906596+Aviraj0714@users.noreply.github.com>
2024-12-04 19:10:48 +0530

    home screen

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,13 +1,14 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const Home = () => {
   return (
     <div>
-        <div className='h-screen flex justify-between flex-col w-full bg-red-400'>
-            <img src="https://banner2.cleanpng.com/lnd/20241123/xu/01a0c7a4bc31fd14d50f86a45d55c0.webp" alt="" />
-<div className='bg-white '>
-    <h2>Get Started With Uber</h2>
-    <button>Continue</button>
+        <div className='h-screen bg-center bg-cover pt-8 flex justify-between flex-col w-full bg-[url(https://images.unsplash.com/photo-1557404763-69708cd8b9ce?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dHJhZmZpYyUyMGxpZ2h0fGVufDB8fDB8fHww)]'>
+            <img className='w-20 ml-5 ' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+<div className='bg-white pb-7 py-4 px-4 '>
+    <h2 className='text-3xl font-bold'>Get Started With Uber</h2>
+    <Link to='/login' className='w-full flex items-center justify-center bg-black text-white py-3 mt-5 rounded'>Continue</Link>
 </div>
         </div>
     </div>
