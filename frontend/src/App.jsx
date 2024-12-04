7b3b38080438d19b245fe5c0c3728d73213ffd29
Aviraj0714 <131906596+Aviraj0714@users.noreply.github.com>
2024-12-04 12:51:51 +0530

    ui part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/home'
+import Userlogin from './pages/userlogin'
+import Usersignup from './pages/usersignup'
+import Captainlogin from './pages/captainlogin'
+import Captainsignup from './pages/captainsignup'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/login' element={<Userlogin/>}></Route>
+        <Route path='/signup' element={<Usersignup/>}></Route>
+        <Route path='/captain-login' element={<Captainlogin/>}></Route>
+        <Route path='/captain-signup' element={<Captainsignup/>}></Route>
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div className='h-screen flex justify-between flex-col w-full bg-red-400'>
+            <img src="https://banner2.cleanpng.com/lnd/20241123/xu/01a0c7a4bc31fd14d50f86a45d55c0.webp" alt="" />
+<div className='bg-white '>
+    <h2>Get Started With Uber</h2>
+    <button>Continue</button>
+</div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
