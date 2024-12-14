00dc702c5429a5659dfb0ddd81b9543a6d9916c5
Aviraj0714 <131906596+Aviraj0714@users.noreply.github.com>
2024-12-14 18:14:31 +0530

    ride section

--- frontend/src/components/locationpanel.jsx
+++ frontend/src/components/locationpanel.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
-
+import 'remixicon/fonts/remixicon.css'
 const Locationpanel = () => {
   return (
-    <div>locationpanel</div>
+   <div className='ml-4'>
+     <div className='flex items-center my-4 gap-4 justify-start'>
+      <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center  rounded-full'>
+        <i className='ri-map-pin-fill'></i>
+      </h2>
+      <h4 className='font-medium'>
+        near,mahakal mandir
+      </h4>
+    </div>
+
+    <div className='flex items-center my-4 gap-4 justify-start'>
+      <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center  rounded-full'>
+        <i className='ri-map-pin-fill'></i>
+      </h2>
+      <h4 className='font-medium'>
+        near,mahakal mandir
+      </h4>
+    </div>
+
+    <div className='flex items-center my-4 gap-4 justify-start'>
+      <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center  rounded-full'>
+        <i className='ri-map-pin-fill'></i>
+      </h2>
+      <h4 className='font-medium'>
+        near,mahakal mandir
+      </h4>
+    </div>
+
+    <div className='flex items-center my-4 gap-4 justify-start'>
+      <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center  rounded-full'>
+        <i className='ri-map-pin-fill'></i>
+      </h2>
+      <h4 className='font-medium'>
+        near,mahakal mandir
+      </h4>
+    </div>
+
+    <div className='flex items-center my-4 gap-4 justify-start'>
+      <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center  rounded-full'>
+        <i className='ri-map-pin-fill'></i>
+      </h2>
+      <h4 className='font-medium'>
+        near,mahakal mandir
+      </h4>
+    </div>
+
+   </div>
   )
 }
 
